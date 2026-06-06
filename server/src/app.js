008067e17423f681f28989e8906ff6de9a418776
Anurag Kumar <ak7546824@gmail.com>
2026-06-06 10:49:52 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const cors = require("cors");
 
 const notesRoutes = require("./routes/notesRoutes");
-const app = express();
 const errorHandler = require("./middleware/errorHandler");
 
-app.use(errorHandler);
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "NoteFlow API is running",
+  });
+});
+
 app.use("/api/notes", notesRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
